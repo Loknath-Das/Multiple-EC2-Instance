67a6f1e2f6a9a3709390e98e695f36aa4689fae1
Loknath-Das <loknathdas183@gmail.com>
2022-06-16 20:36:36 +0530

    minor change

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region   = var.aws_region["virginia"] # region names can be found in variables.tf file
-  profile  = "terraform"           # changeable as per user configuration
+  profile  = "default"           # changeable as per user configuration
 }
